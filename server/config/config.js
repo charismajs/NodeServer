438b907919b36268fbc7958ddd06350b79b2a861
LuckyJS <charismajs@gmail.com>
2014-08-23 10:14:10 +0900

    add ip module for get a server network ip

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,14 @@
 /**
  * Created by charismajs on 2014-07-08.
  */
+var ip = require('ip');
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
-
-var network = require('./../services/network');
-
 
 module.exports = {
   development: {
 //    server: 'http://192.168.246.10:5984', // youngmin
-    server: network.getNetworkIP('en0', 'IPv4', false), // jinsu
+    server: ip.address(), // jinsu
     port: process.env.PORT || 3030,
     db: 'http://192.168.246.10', // youngmin
 //    db: 'http://192.168.246.11', // jinsu
