f8363a8cae4c3cfbcf25c6bcc063c7c3060fabf6
LuckyJS <charismajs@gmail.com>
2015-01-08 10:10:26 +0900

    ip setting (localhost only)

--- server/config/constants/daConstants.js
+++ server/config/constants/daConstants.js
@@ -5,7 +5,7 @@ module.exports = {
     version: '1.0'
   },
   database: {
-//    ip: '127.0.0.1',
+    ip: '127.0.0.1',
 //    ip: '192.168.246.10', //Youngmin
 //    ip: '192.168.246.11', //JInsu
 //    ip: '192.168.246.31', //Yongin
